2368d072c4a5c3222d0b8712135c5da01f34e7b1
Rigo <limon1285@gmail.com>
2021-01-06 21:52:00 -0600

    added pg production to knex

--- knexfile.js
+++ knexfile.js
@@ -35,4 +35,15 @@ module.exports = {
       },
     },
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 };

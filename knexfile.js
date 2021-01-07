1c3b07798fa1db96d0bf12416f6b51b189373bee
Rigo <limon1285@gmail.com>
2021-01-06 22:50:56 -0600

    api auth/register/login endpoints

--- knexfile.js
+++ knexfile.js
@@ -38,6 +38,10 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
       directory: "./database/migrations",
       tableName: "knex_migrations",

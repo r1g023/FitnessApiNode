97eba207dd0f5b04a7f63b51366b13d67928dd4f
Rigo <limon1285@gmail.com>
2021-01-07 20:42:43 -0600

    removed production to have my api working for now and use pg on a diff branch

--- knexfile.js
+++ knexfile.js
@@ -35,15 +35,21 @@ module.exports = {
       },
     },
   },
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./database/migrations",
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-  },
+  // production: {
+  //   client: "pg",
+  //   connection: process.env.DATABASE_URL,
+  //   migrations: {
+  //     directory: "./database/migrations",
+  //     tableName: "knex_migrations",
+  //   },
+  //   seeds: {
+  //     directory: "./database/seeds",
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, done) => {
+  //       // runs after a connection is made to the sqlite engine
+  //       conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+  //     },
+  //   },
+  // },
 };

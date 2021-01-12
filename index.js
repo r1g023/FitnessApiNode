bd2f65f07fb3ab15cc5811f8a5dd892bc0a91423
Rigo <limon1285@gmail.com>
2021-01-11 23:09:03 -0600

    update

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require("./api/server.js");
 // port 5000
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.log(`server is listening on port ${port}...`);
+  console.log(`server is listening on port ${port}....`);
 });

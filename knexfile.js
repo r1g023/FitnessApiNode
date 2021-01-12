220cf45a82f4ef1618b8fbb4a6395536f3432c82
Rigo <limon1285@gmail.com>
2021-01-11 22:58:02 -0600

    added postgrest for production

--- knexfile.js
+++ knexfile.js
@@ -35,21 +35,20 @@ module.exports = {
       },
     },
   },
-  // production: {
-  //   client: "pg",
-  //   connection: process.env.DATABASE_URL,
-  //   migrations: {
-  //     directory: "./database/migrations",
-  //     tableName: "knex_migrations",
-  //   },
-  //   seeds: {
-  //     directory: "./database/seeds",
-  //   },
-  //   pool: {
-  //     afterCreate: (conn, done) => {
-  //       // runs after a connection is made to the sqlite engine
-  //       conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
-  //     },
-  //   },
-  // },
+  production: {
+    client: "pg",
+    //automatically configured in heroku config vars
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./database/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
 };

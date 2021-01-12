d323897131a8b7ab888539350f2d45bcddad938c
Rigo <limon1285@gmail.com>
2021-01-11 23:24:00 -0600

    pg

--- knexfile.js
+++ knexfile.js
@@ -37,18 +37,19 @@ module.exports = {
   },
   production: {
     client: "pg",
-    //automatically configured in heroku config vars
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./database/migrations",
-      // tableName: "knex_migrations",
+      tableName: "knex_migrations",
     },
     seeds: {
       directory: "./database/seeds",
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     // runs after a connection is made to the sqlite engine
+    //     conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+    //   },
+    // },
   },
 };
